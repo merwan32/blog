199127f4c32bdf59936af3d36b7465248bb3113f
merwan bekkar <Bkar.mrwan32@gmail.com>
2022-08-15 14:51:39 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+def search(request):
+    return render(request,'search.html')
+
+def detail(request):
+    return render(request,'detail.html')
