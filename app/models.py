c84d1e7656323580f8349481c4ff65674dca7fff
merwan bekkar <Bkar.mrwan32@gmail.com>
2022-08-16 17:24:18 +0200

    search

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    work = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to='profilepics',default='profilepics/default-user.png')
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    type = models.CharField(max_length=100, blank=True)
+    titre = models.CharField(max_length=500, blank=True)
+    text = models.CharField(max_length=1000, blank=True)
+    time = models.DateTimeField(auto_now=True)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from .models import Profile,Post
+
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    obj= Post.objects.all().order_by('-id')
+    head = obj[0]
+    posts = obj[1:7]
+    return render(request,'home.html',{'posts':posts,'head':head})
+
+
+def blogs(request):
+    posts= Post.objects.all().order_by('-id')
+    return render(request,'blogs.html',{'posts':posts})
 
 
 def search(request):
-    return render(request,'search.html')
+    search = request.GET['search']
+    posts = Post.objects.filter(titre__icontains=search)
+    return render(request,'search.html',{'search':search,'posts':posts})
 
 def detail(request):
     return render(request,'detail.html')
